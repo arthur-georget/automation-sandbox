87caca65a218128da8e324c9944339a90c167238
arthur-georget <arthur.georget@laplateforme.io>
2026-06-24 13:38:40 +0200

    feat(CalciferProcess): Github API connection set in CalciferProcess constructor. CalciferProcess now instantiated and connection status fetched in CalciferAPI.codeReviewed(). fix(Payara): fixed Jackson version mismatch between Payara and Github-API inverting dependencies loading order.

--- sandbox/src/main/java/com/automation/CalciferAPI.java
+++ sandbox/src/main/java/com/automation/CalciferAPI.java
@@ -4,11 +4,20 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.io.IOException;
+
 @Path("/codeReviewed")
 public class CalciferAPI {
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     public String codeReviewed() {
-        return "HTTP request successfully received. Calcifer will now asks Github to merge concerned branch to preprod and then build it.";
+        CalciferProcess calciferProcess = new CalciferProcess();
+        String githubStatus = "";
+        try {
+            githubStatus = calciferProcess.getGithubStatus();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return "HTTP request successfully received. Github status: " + githubStatus;
     }
 }
